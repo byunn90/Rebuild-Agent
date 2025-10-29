9e692e2c32cd7876d3a919d08f27fe5ea36ac221
Kayhan Mamak <kayhan.mamak@hotmail.com>
2025-10-29 20:59:39 +1100

    sys.argv working

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import os
+import sys
 from google import genai
 from dotenv import load_dotenv
 
-load_dotenv()
 
-api_key = os.environ.get("GEMINI_API_KEY")
 
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
+    prompt_control = sys.argv[0]
+    api_key = os.environ.get("GEMINI_API_KEY")
 
-response = client.models.generate_content(
-    model='gemini-2.5-flash', 
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=prompt_control
 
-    
-)
+        
+    )
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
 
-print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-print("Response tokens:", response.usage_metadata.candidates_token_count)
-print("Response:")
-print(response.text)
+    if len(sys.argv) < 2:
+        print("Error has occured")
+        sys.exit(1)
+    print(prompt_control)    
+
+
+if __name__ == "__main__":
+    main()
