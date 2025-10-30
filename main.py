719e9cf4d616efb6dec88b28f051f462b0ef54ee
Kayhan Mamak <kayhan.mamak@hotmail.com>
2025-10-30 18:26:15 +1100

    calculator folder, render done

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 
 def main():
     load_dotenv()
-    prompt_control = sys.argv[0]
+    is_verbose = "--verbose" in sys.argv 
     api_key = os.environ.get("GEMINI_API_KEY")
-
+    prompt_control = sys.argv[0]
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
@@ -15,18 +15,19 @@ def main():
 
         
     )
-    # print("Response:")
-    # print(response.text)
-    
     if len(sys.argv) < 2:
         print("Error has occured")
-        sys.exit(1)
-    if "--verbose":    
-        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print("Response tokens:", response.usage_metadata.candidates_token_count)
-        print(f"User prompt:" {[prompt]})
+        sys.exit(1)    
+    if "--verbose" in sys.argv:    
+        print(f"User prompt: {prompt_control}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(response.text)
+    print("Error No --verbose")
+
     
-    print(prompt_control)    
+    
+
 
 
 if __name__ == "__main__":
