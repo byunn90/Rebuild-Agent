9844a2b9f346b852db9a4c8441fae8d28d5e0628
Kayhan Mamak <kayhan.mamak@hotmail.com>
2025-11-01 19:59:31 +1100

    get_file_info finished

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -5,21 +5,27 @@ from dotenv import load_dotenv
 
 
 def get_files_info(working_directory, directory="."):
-    finding_directory = os.path.abspath(os.path.join(working_directory, directory))
-    get_contents_of_path = os.listdir(finding_directory)
-    new_array = []
-    # What we need now 
-    # - README.md: file_size=1032 bytes, is_dir=False
-    # - src: file_size=128 bytes, is_dir=True
-    # - package.json: file_size=1234 bytes, is_dir=False
-    if not finding_directory.startswith(working_directory): 
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(directory):
+    results = []
+    # Get Path
+    get_path = os.path.abspath(os.path.join(working_directory, directory))
+    if not get_path.startswith(os.path.abspath(working_directory)):
+         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(get_path):
         return f'Error: "{directory}" is not a directory'
-    for i in get_contents_of_path:
-        print(i)
-        new_array.append(i)
-    return print(new_array)    
+    # Get content 
+    get_contents = os.listdir(get_path)    
+    for item in get_contents:
+        full_path = os.path.join(get_path, item)
+        file_size = os.path.getsize(full_path)
+        is_dir = os.path.isdir(full_path)
+        line = f"- {item}: file_size={file_size} bytes, is_dir={is_dir}"
+        results.append(line)
+    return "\n".join(results)
+
+get_files_info("calculator", ".")
+
+
+
 
     
 
