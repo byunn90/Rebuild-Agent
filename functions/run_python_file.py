a8fce4d7d54059519afafb0f19525a62eed1d16b
Kayhan Mamak <kayhan.mamak@hotmail.com>
2025-11-04 19:51:54 +1100

    run_python working

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,20 +1,40 @@
 import os
-
+import subprocess
 
 def run_python_file(working_directory, file_path, args=[]):
     get_path = os.path.abspath(os.path.join(working_directory, file_path))
 
     if not get_path.startswith(os.path.abspath(working_directory)):
         return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
-    if not os.path.exists(get_path): 
-        return print(f"Error: {file_path}") 
+    if not os.path.isfile(get_path):
+         return f'Error: File "{file_path}" not found.'
     if not get_path.endswith(".py"):  
         return f'Error: "{file_path}" is not a Python file.'
-    x = subprocess.run(
-        args,
-        stdin=None, 
-        input=None, 
-        stdout=None,
-        timeout=30
-    )       
-    return x    
+    try:    
+        processes = subprocess.run(
+            ["python3", get_path, *args],
+            capture_output=True, 
+            text=True,             
+            timeout=30,
+            cwd=working_directory
+         )
+        stdout_data = processes.stdout
+        stderr_data = processes.stderr
+        exit_code = processes.returncode
+        if not stdout_data and not stderr_data:
+            return "No output produced"
+
+        result = f"STDOUT:\n{stdout_data}\nSTDERR:\n{stderr_data}"
+        if exit_code != 0:
+            result += f"\nProcess exited with code {exit_code}"
+        return result.strip()
+    except Exception as e:
+        return f"Error: executing Python file: {e}"    
+
+    
+
+
+   
+ 
+
+      
--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import os
 import sys
+from google.genai import types
 from google import genai
 from dotenv import load_dotenv
 
 def main():
     load_dotenv()
     is_verbose = "--verbose" in sys.argv 
+    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
     api_key = os.environ.get("GEMINI_API_KEY")
-    prompt_control = sys.argv[0]
+    prompt_control = sys.argv[1]
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt_control
-
-        
+        contents=prompt_control,
+        config=types.GenerateContentConfig(system_instruction=system_prompt)   
     )
     if len(sys.argv) < 2:
         print("Error has occured")
@@ -24,6 +25,7 @@ def main():
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
         print(response.text)
     print("Error No --verbose")
+    print(response.text)
 
     
 if __name__ == "__main__":
