747fb3b9023b563ec311a8d1680640f0d17480be
Kayhan Mamak <kayhan.mamak@hotmail.com>
2025-11-04 20:21:37 +1100

    added schema get file info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,30 +1,37 @@
 import os
-from google import genai
-from dotenv import load_dotenv
-
-
+from google.genai import types
 
 def get_files_info(working_directory, directory="."):
     results = []
-    # Get Path
     get_path = os.path.abspath(os.path.join(working_directory, directory))
+
     if not get_path.startswith(os.path.abspath(working_directory)):
-         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     if not os.path.isdir(get_path):
         return f'Error: "{directory}" is not a directory'
-    # Get content 
-    get_contents = os.listdir(get_path)    
-    for item in get_contents:
+
+    for item in os.listdir(get_path):
         full_path = os.path.join(get_path, item)
         file_size = os.path.getsize(full_path)
         is_dir = os.path.isdir(full_path)
-        line = f"- {item}: file_size={file_size} bytes, is_dir={is_dir}"
-        results.append(line)
+        results.append(f"- {item}: file_size={file_size} bytes, is_dir={is_dir}")
+
     return "\n".join(results)
 
-get_files_info("calculator", ".")
 
-
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
 
 
     
--- main.py
+++ main.py
@@ -3,19 +3,31 @@ import sys
 from google.genai import types
 from google import genai
 from dotenv import load_dotenv
-
+from functions.get_files_info import schema_get_files_info
 def main():
     load_dotenv()
     is_verbose = "--verbose" in sys.argv 
-    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
+    available_functions = types.Tool(function_declarations=[schema_get_files_info])
     api_key = os.environ.get("GEMINI_API_KEY")
     prompt_control = sys.argv[1]
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=prompt_control,
-        config=types.GenerateContentConfig(system_instruction=system_prompt)   
-    )
+        config=types.GenerateContentConfig(
+        tools=[available_functions],
+        system_instruction=system_prompt
+    ),
+)
     if len(sys.argv) < 2:
         print("Error has occured")
         sys.exit(1)    
