41be46a1a7afc44b0e1ee2670fb3a560531fc22e
Kayhan Mamak <kayhan.mamak@hotmail.com>
2025-11-06 20:56:38 +1100

    function dispatcher

--- functions/call_function.py
+++ functions/call_function.py
@@ -6,4 +6,24 @@ from write_file import schema_write_file
 
 def call_function(function_call_part, verbose=False):
 
+    function_map = {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "run_python_file":  run_python_file,
+        "write_file": write_file
+
+    }
+    if verbose == True:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")   
     
+    function_name = function_call_part.name
+    function_arg = function_call_part.args
+
+
+    
+
+
+    
+
