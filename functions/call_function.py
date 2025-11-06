6c0e7b2d1d8a0e09a36a3416071e2af153040b38
Kayhan Mamak <kayhan.mamak@hotmail.com>
2025-11-06 21:01:42 +1100

    underhood LLM

--- functions/call_function.py
+++ functions/call_function.py
@@ -19,10 +19,15 @@ def call_function(function_call_part, verbose=False):
         print(f" - Calling function: {function_call_part.name}")   
     
     function_name = function_call_part.name
-    function_arg = function_call_part.args
+    function_arg = dict(function_call_part.args or {})
+    fn = function_map.get(function_name)
+
+    function_arg["working_directory"] = "calculator"
+
+    result = fn(**function_arg)
 
 
-    
+
 
 
     
