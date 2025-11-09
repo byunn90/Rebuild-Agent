dd5ef500478848dde87e5e1c7c164eff5cee5d12
Kayhan Mamak <kayhan.mamak@hotmail.com>
2025-11-10 06:08:36 +1100

    replace notes

--- functions/call_function.py
+++ functions/call_function.py
@@ -19,7 +19,7 @@ def call_function(function_call_part, verbose=False):
         print(f" - Calling function: {function_call_part.name}")   
     
     function_name = function_call_part.name
-    #  Go over this please
+    #  Dont forget to use types.content
     function_arg = dict(function_call_part.args or {})
     fn = function_map.get(function_name)
 
